4e75370e5adb6ab6ace04c9b8189bd50fbe76e63
976623696@qq.com <LZJoppoA201>
2020-02-06 19:05:31 +0800

    首页及分类页

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,3 +1,5 @@
+import { request } from "../../request/index.js";
+
 // pages/category/index.js
 Page({
 
@@ -5,62 +7,61 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    leftMenuList: [],
+    rightContent: [],
+    activeIndex: 0,
+    scrollTop: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    let cates = wx.getStorageSync("cates");
+    if (cates) {
+      if (Date.now() - cates._time > 5 * 60 * 1000) {
+        this.getMenuList();
+      } else {
+        this.Cates = cates._data;
+        this.setData({
+          leftMenuList: this.Cates.map(
+            v => v.cat_name
+          ),
+          rightContent: this.Cates[0].children
+        });
+      }
+    } else {
+      this.getMenuList();
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  //定义接口返回值
+  Cates: [],
 
+  getMenuList() {
+    request({
+      url: "https://api.zbztb.cn/api/public/v1/categories"
+    }).then((result) => {
+      this.Cates = result.data.message;
+      wx.setStorageSync("cates", {
+        _time: Date.now(),
+        _data: result.data.message
+      });
+      this.setData({
+        leftMenuList: this.Cates.map(
+          v => v.cat_name
+        ),
+        rightContent: this.Cates[0].children
+      });
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleItem(e) {
+    let index = e.currentTarget.dataset.index;
+    this.setData({
+      activeIndex: index,
+      rightContent: this.Cates[index].children,
+      scrollTop: 0
+    });
   }
+
 })
